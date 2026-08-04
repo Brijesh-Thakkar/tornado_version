50303999060dddcbe16cf5485f901d7a4adf36f0
Brijesh-Thakkar <brijeshthakkariitian5126@gmail.com>
2026-08-05 00:42:16 +0530

    data and assets added

--- website/app/controllers/main.py
+++ website/app/controllers/main.py
@@ -25,9 +25,12 @@ define("port", default=8888, help="run on the given port", type=int)
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            #(r"/(.*)", WebHomeHandler),
             (r"/", tornado.web.RedirectHandler, {"url": "/web/home/index.html"}),
-            (r"/web/home/(.*)", WebHomeHandler)
+            (r"/web/login", UserLoginHandler),
+            (r"/web/logout", UserLogoutHandler),
+            (r"/web/register", UserRegisterHandler),
+            (r"/web/lostpw", UserLostPasswordHandler),
+            (r"/web/home/(.*)", WebHomeHandler),
         ]
         settings = dict(
             app_title=u"Aspiring Apps",
@@ -35,8 +38,9 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "../public"),
             static_url_prefix="/web/static/",
             xsrf_cookies=False,
-            cookie_secret=os.environ.get("COOKIE_SECRET"),
-            login_url="/"            
+            cookie_secret=os.environ.get("COOKIE_SECRET", "11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="),
+            login_url="/",
+            debug=True,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
         
@@ -53,8 +57,50 @@ class BaseHandler(tornado.web.RequestHandler):
 class WebHomeHandler(BaseHandler):
     def get(self, slug):
         logging.info("slug is: %r" % slug)
-        # does slug file exist
-        self.render(slug)        
+        try:
+            self.render(slug)
+        except Exception as e:
+            logging.error("Error rendering %s: %s" % (slug, str(e)))
+            self.redirect("/web/home/log-in.html")
 
-        
+    def post(self, slug):
+        logging.info("slug is: %r" % slug)
+        try:
+            self.render(slug)
+        except Exception as e:
+            logging.error("Error rendering %s: %s" % (slug, str(e)))
+            self.redirect("/web/home/log-in.html")
 
+
+class UserLoginHandler(BaseHandler):
+    def get(self):
+        user = self.get_current_user()
+        if user is None:
+            self.redirect("/web/home/log-in.html")
+        else:
+            self.redirect("/web/home/index.html")
+
+    def post(self):
+        self.redirect("/web/home/log-in.html")
+
+
+class UserLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect("/web/home/log-in.html")
+
+
+class UserRegisterHandler(BaseHandler):
+    def get(self):
+        self.redirect("/web/home/register.html")
+
+    def post(self):
+        self.redirect("/web/home/register.html")
+
+
+class UserLostPasswordHandler(BaseHandler):
+    def get(self):
+        self.redirect("/web/home/log-in.html")
+
+    def post(self):
+        self.redirect("/web/home/log-in.html")
